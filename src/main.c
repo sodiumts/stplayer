b4c26c20b23ea76a9d5c9d32512c5ae11a313135
sodiumts <ernests.malnacs@gmail.com>
2025-08-09 22:45:46 +0200

    Made the flash be read only for the moment

--- src/main.c
+++ src/main.c
@@ -76,18 +76,16 @@ int stream_wav(const char *path) {
     }
     
     uint16_t packet_size;
-    uint32_t packet_cnt = 0;
     while(1) {
         void * block = NULL;
         //LOG_INF("Getting packet");
-        rc = opus_get_packet(&op_state, opus_packet, &packet_size, &filep);
+        int rcf = opus_get_packet(&op_state, opus_packet, &packet_size, &filep);
         // Failed
-        if (rc != OP_OK && rc != OP_DONE)
+        if (rcf != OP_OK && rcf != OP_DONE)
             goto cleanup;
 
         //LOG_INF("Allocating block");
 
-        packet_cnt++;
         rc = k_mem_slab_alloc(&tx_0_mem_slab, &block, K_FOREVER);
         if (rc < 0) {
             LOG_ERR("Block allocation failed: %d", rc);
@@ -102,7 +100,7 @@ int stream_wav(const char *path) {
         }
         
         //LOG_INF("Decoded count: %d", oprc);
-        //apply_volume_int16((int16_t *) block, oprc, 5);
+        apply_volume_int16((int16_t *) block, oprc, 5);
         
         //k_sleep(K_MSEC(50));
         rc = i2s_write(i2s_dev, block, BLOCK_SIZE);
@@ -115,143 +113,12 @@ int stream_wav(const char *path) {
 
 
         // Done with file
-        if (rc == OP_DONE) {
+        if (rcf == OP_DONE) {
             LOG_INF("Done with file");
             goto cleanup;
         }
     }
-  //  uint8_t riff_header[12];
-  //  uint32_t data_size = 0;
-  //  uint32_t sample_rate = 0;
-  //  uint16_t bits_per_sample = 0;
-  //  uint16_t num_channels = 0;
-
-  //  if (fs_read(&filep, riff_header, sizeof(riff_header)) != sizeof(riff_header)) {
-  //      LOG_ERR("Failed to read RIFF header");
-  //      rc = -EIO;
-  //      goto cleanup;
-  //  }
-
-  //  if (memcmp(riff_header, "RIFF", 4) != 0 || memcmp(riff_header + 8, "WAVE", 4) != 0) {
-  //      LOG_ERR("Not a valid WAV file");
-  //      rc = -EINVAL;
-  //      goto cleanup;
-  //  }
-
-  //  while (1) {
-  //      char chunk_id[4];
-  //      uint32_t chunk_size;
-  //      
-  //      if (fs_read(&filep, chunk_id, 4) != 4) {
-  //          LOG_ERR("Failed to read chunk ID");
-  //          rc = -EIO;
-  //          goto cleanup;
-  //      }
-
-  //      if (fs_read(&filep, &chunk_size, 4) != 4) {
-  //          LOG_ERR("Failed to read chunk size");
-  //          rc = -EIO;
-  //          goto cleanup;
-  //      }
-
-  //      if (memcmp(chunk_id, "fmt ", 4) == 0) {
-  //          struct __attribute__((packed)) {
-  //              uint16_t audio_format;
-  //              uint16_t num_channels;
-  //              uint32_t sample_rate;
-  //              uint32_t byte_rate;
-  //              uint16_t block_align;
-  //              uint16_t bits_per_sample;
-  //          } fmt_chunk;
-
-  //          if (fs_read(&filep, &fmt_chunk, sizeof(fmt_chunk)) != sizeof(fmt_chunk)) {
-  //              LOG_ERR("Failed to read fmt chunk");
-  //              rc = -EIO;
-  //              goto cleanup;
-  //          }
-
-  //          if (chunk_size > sizeof(fmt_chunk)) {
-  //              fs_seek(&filep, chunk_size - sizeof(fmt_chunk), FS_SEEK_CUR);
-  //          }
-
-  //          if (fmt_chunk.audio_format != 1) {  // 1 = PCM
-  //              LOG_ERR("Only PCM format supported");
-  //              rc = -ENOTSUP;
-  //              goto cleanup;
-  //          }
-  //          
-  //          num_channels = fmt_chunk.num_channels;
-  //          sample_rate = fmt_chunk.sample_rate;
-  //          bits_per_sample = fmt_chunk.bits_per_sample;
-  //      }
-  //      else if (memcmp(chunk_id, "data", 4) == 0) {
-  //          data_size = chunk_size;
-  //          break;
-  //      }
-  //      else {
-  //          fs_seek(&filep, chunk_size, FS_SEEK_CUR);
-  //      }
-  //  }
-
-  //  if (num_channels != CHANNELS) {
-  //      LOG_ERR("Unsupported number of channels: %d (expected %d)", 
-  //              num_channels, CHANNELS);
-  //      rc = -ENOTSUP;
-  //      goto cleanup;
-  //  }
-
-  //  if (bits_per_sample != 16) {
-  //      LOG_ERR("Only 16-bit samples supported");
-  //      rc = -ENOTSUP;
-  //      goto cleanup;
-  //  }
-
-  //  if (sample_rate != 48000) {
-  //      LOG_WRN("Sample rate mismatch: file=%dHz, configured=48000Hz", sample_rate);
-  //  }
-
-  //  LOG_INF("Channels: %d bits per sample: %d sample rate %d", num_channels, bits_per_sample, sample_rate);
-
-  //  size_t bytes_remaining = data_size;
-  //  while (bytes_remaining > 0) {
-  //      void *block = NULL;
-  //      ssize_t read_n = 0;
-
-  //      rc = k_mem_slab_alloc(&tx_0_mem_slab, &block, K_FOREVER);
-  //      if (rc < 0) {
-  //          LOG_ERR("Block allocation failed: %d", rc);
-  //          break;
-  //      }
-
-  //      size_t to_read = MIN(BLOCK_SIZE, bytes_remaining);
-  //      read_n = fs_read(&filep, block, to_read);
-  //      if (read_n < 0) {
-  //          LOG_ERR("Read failed: %d", (int)read_n);
-  //          k_mem_slab_free(&tx_0_mem_slab, &block);
-  //          rc = (int)read_n;
-  //          break;
-  //      }
-
-  //      if ((size_t)read_n < BLOCK_SIZE) {
-  //          memset((uint8_t *)block + read_n, 0, BLOCK_SIZE - (size_t)read_n);
-  //      }
-
-  //      size_t nsamples = BLOCK_SIZE / sizeof(int16_t);
-  //      apply_volume_int16((int16_t *) block, nsamples, 5);
-
-  //      rc = i2s_write(i2s_dev, block, BLOCK_SIZE);
-  //      if (rc < 0) {
-  //          LOG_ERR("i2s_write failed: %d", rc);
-  //          k_mem_slab_free(&tx_0_mem_slab, &block);
-  //          break;
-  //      }
-
-  //      /* success: subtract bytes read */
-  //      bytes_remaining -= read_n;
-  //  }
-
-  //  LOG_INF("Streamed %u bytes of PCM data", data_size - bytes_remaining);
-cleanup:
+  cleanup:
     fs_close(&filep);
     return rc;
 }
