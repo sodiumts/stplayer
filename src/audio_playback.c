7f0937567c670845b4e8f702372783262e816880
sodiumts <ernests.malnacs@gmail.com>
2026-01-17 21:30:18 +0100

    Added tlv320dac for the i2s

--- src/audio_playback.c
+++ src/audio_playback.c
@@ -3,11 +3,13 @@
 #include "syscalls/kernel.h"
 #include "zephyr/kernel.h"
 
+#include <math.h>
 #include <string.h>
 #include <zephyr/logging/log.h>
 #include <opus.h>
 #include <zephyr/drivers/dma.h>
 #include <zephyr/drivers/i2s.h>
+#include <zephyr/audio/codec.h>
 
 LOG_MODULE_REGISTER(audio_playback, LOG_LEVEL_DBG);
 
@@ -19,30 +21,6 @@ static struct fs_file_t filep;
 OpusDecoder *decoder;
 
 K_MEM_SLAB_DEFINE_STATIC(tx_0_mem_slab, WB_UP(BLOCK_SIZE), NUM_BLOCKS, 32);
-
-static void apply_volume_int16(int16_t *samples, size_t nsamples, uint16_t adc_value)
-{
-    const uint16_t ADC_MAX = 4000;
-    const float MAX_AMPLITUDE = 0.6f; // clamp it to stop loudness
-    
-    if (adc_value == 0) {
-        memset(samples, 0, nsamples * sizeof(int16_t));
-        return;
-    }
-
-    if (adc_value > ADC_MAX) adc_value = ADC_MAX;
-
-    float normalized = (float)adc_value / ADC_MAX;
-    float scale = normalized * normalized;    
-
-    if (scale > MAX_AMPLITUDE) {
-        scale = MAX_AMPLITUDE;
-    }
-
-    for (size_t i = 0; i < nsamples; i++) {
-        samples[i] = (int16_t)(samples[i] * scale);
-    }
-}
 
 static int start_i2s_dma() {
     LOG_INF("Pre-filling DMA buffers");
@@ -101,33 +79,22 @@ void play_opus_packet(bool *isPlaying, int discard_cnt, uint16_t volume) {
         *isPlaying = false;
         return;
     }
-    // Returns count of decoded samples
     int oprc = opus_decode(decoder, opus_packet, packet_size, block, SAMPLE_NO, 0);
-    if (oprc < 0) {
-        LOG_ERR("Opus decode failed: %d", oprc);
-        fs_close(&filep);
-        opus_decoder_ctl(decoder, OPUS_RESET_STATE);
-        stop_i2s_dma();
-        *isPlaying = false;
-        return;
-    }
 
     if (discard_cnt > 0) {
-        if (discard_cnt > SAMPLE_NO) {
-            LOG_ERR("Discard count larger than one decoded buffer: %d vs %d", discard_cnt, SAMPLE_NO);
-            fs_close(&filep);
-            opus_decoder_ctl(decoder, OPUS_RESET_STATE);
-            stop_i2s_dma();
-            *isPlaying = false;
-            return;
+        size_t discard_samples = discard_cnt * 2;
+
+        if (discard_samples > (size_t)oprc) {
+            LOG_ERR("Discard too large");
         }
 
-        memmove((int16_t *) block, (int16_t *) block + discard_cnt, (oprc - discard_cnt)* sizeof(int16_t));
-        oprc -= discard_cnt;
-        discard_cnt = 0;
-    }
+        int16_t *audio_data = (int16_t *)block;
+        memmove(audio_data, 
+                audio_data + discard_samples,  // skip discards
+                (oprc - discard_samples) * sizeof(int16_t));
 
-    apply_volume_int16((int16_t *) block, oprc * 2, volume);
+        oprc -= discard_samples;
+    }
 
     rc = i2s_write(i2s_dev, block, BLOCK_SIZE);
     if (rc < 0) {
@@ -217,7 +184,43 @@ void audio_handler_thread(void *pipeP, void *arg2, void *arg3) {
 
     }
 }
+static const struct device *codec;
+void codec_initialize(void) {
+    codec = DEVICE_DT_GET(DT_NODELABEL(codec0));
+    if(!device_is_ready(codec)) {
+        LOG_ERR("CODEC not ready");
+        return;
+    }
 
+    struct audio_codec_cfg cfg = {0};
+    cfg.dai_route = AUDIO_ROUTE_PLAYBACK;
+    cfg.dai_type = AUDIO_DAI_TYPE_I2S;
+    cfg.mclk_freq = 12288000;
+
+    cfg.dai_cfg.i2s.word_size = 16;
+    cfg.dai_cfg.i2s.channels = CHANNELS;
+    cfg.dai_cfg.i2s.format = I2S_FMT_DATA_FORMAT_I2S | I2S_FMT_DATA_ORDER_MSB;
+    cfg.dai_cfg.i2s.options         = I2S_OPT_FRAME_CLK_SLAVE | I2S_OPT_BIT_CLK_SLAVE;
+    cfg.dai_cfg.i2s.frame_clk_freq = SAMPLE_RATE;
+    cfg.dai_cfg.i2s.block_size = BLOCK_SIZE;
+    cfg.dai_cfg.i2s.mem_slab = &tx_0_mem_slab;
+    
+    int rc = audio_codec_configure(codec, &cfg);
+    if (rc) {
+        LOG_ERR("Audio configure failed: %d", rc);
+        return;
+    }    
+
+
+    audio_codec_start_output(codec);
+    
+    audio_property_value_t v = {.vol = -40};
+    rc = audio_codec_set_property(codec, AUDIO_PROPERTY_OUTPUT_VOLUME, AUDIO_CHANNEL_ALL, v);
+    if(rc) {
+        LOG_ERR("Failed to set volume");
+        return;
+    }
+}
 
 static int configure_i2s() {
     int ret;
@@ -263,5 +266,7 @@ int init_audio_playback() {
     if (ret < 0) {
         return ret;
     }
+
+    codec_initialize();
     return 0;
 }
